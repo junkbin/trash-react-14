64677a7bd5efa7125befc1a19fe049389c5054a9
santosh <santosh.ece06@gmail.com>
2018-11-09 23:16:58 +0530

    live build

---
+++ build/precache-manifest.5776605bace281ee23de9234379f65be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed02db70a2a8d8927475",
+    "url": "/static/js/main.ed02db70.chunk.js"
+  },
+  {
+    "revision": "4f541c065993f9b6a102",
+    "url": "/static/js/1.4f541c06.chunk.js"
+  },
+  {
+    "revision": "ed02db70a2a8d8927475",
+    "url": "/static/css/main.407594e7.chunk.css"
+  },
+  {
+    "revision": "eb164727dc63b01ce437d843d665bd86",
+    "url": "/index.html"
+  }
+];
