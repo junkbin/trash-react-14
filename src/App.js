7c3e4f19e6f6df4f143a8cbd07afac1652957056
santosh <santosh.ece06@gmail.com>
2018-11-09 22:39:14 +0530

    trash-react-14

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    const finalElement = (
+      <div className="bg-primary" >
+        <Welcome id="AFF0001" name="Affixussss"/>
       </div>
     );
+
+    return finalElement;
   }
 }
+
+
+function Welcome(props){
+  return (
+    <div>
+      <hr />
+      <h1>Welcome, Welcomes Everyone !!! </h1>
+      <h1>{props.id}</h1>
+      <h1>{props.name}</h1>
+      <h1>{new Date().toLocaleTimeString()}</h1>
+    </div>
+  );
+}
+
+
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const element = (
+    <div>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, corporis?</h2>
+    </div>
+);
+
+ReactDOM.render(element, document.getElementById('root2'));
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+setInterval(()=>{
+    ReactDOM.render(<App />, document.getElementById("root1"));
+}, 1000);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
