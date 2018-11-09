cd40e90152ef2b0eb7cb2fe2781288d997cb5e9f
santosh <santosh.ece06@gmail.com>
2018-11-09 23:51:25 +0530

    live

---
+++ docs/precache-manifest.791151c85508893d5132b36f495bc08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./trash-react-14/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "813a636949a735f20d3b",
+    "url": "./trash-react-14/static/js/runtime~main.813a6369.js"
+  },
+  {
+    "revision": "58b067f41de173703920",
+    "url": "./trash-react-14/static/js/main.58b067f4.chunk.js"
+  },
+  {
+    "revision": "4f541c065993f9b6a102",
+    "url": "./trash-react-14/static/js/1.4f541c06.chunk.js"
+  },
+  {
+    "revision": "58b067f41de173703920",
+    "url": "./trash-react-14/static/css/main.407594e7.chunk.css"
+  },
+  {
+    "revision": "327f88266ce2d4e7788310e8b8792710",
+    "url": "./trash-react-14/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d178d45f194011c725e517d2883944f1.js"
+  "./trash-react-14/precache-manifest.791151c85508893d5132b36f495bc08c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("./trash-react-14/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
